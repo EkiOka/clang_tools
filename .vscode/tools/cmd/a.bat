c2da229858720528fc47c2859d1c98749f580c6a
EkiOka <96645770+EkiOka@users.noreply.github.com>
2023-05-11 21:42:09 +0900

    バッチ処理修正

--- .vscode/tools/cmd/a.bat
+++ .vscode/tools/cmd/a.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 rem ----------------------------------------------------------------------
 rem doxygen
@@ -6,6 +6,8 @@ rem ----------------------------------------------------------------------
 
 rem run doxygen
 doxygen.exe "%path_doxygen_file%"
+
+call fl.bat
 
 rem build makefile
 py "%gen_doxygen_mk%" "-src_path:%path_doxygen_xml_files%" "-tmp_path:%tmp_doxygen_mk%" "-dest_path:%out_doxygen_makefile%"
--- .vscode/tools/hook/vscode_terminal.bat
+++ .vscode/tools/hook/vscode_terminal.bat
@@ -46,15 +46,23 @@ set "out_doxygen_makefile=%out_doxygen_mk%\doxygen.mk"
 set "out_doxygen_result_file=%out_doxygen_result%\result.yml"
 
 rem --------------------------------------------------------------------------
-rem make directory
+rem remake directory
 rem --------------------------------------------------------------------------
 
-md %out_doxygen_root%
-md %out_doxygen_mk%
-md %out_doxygen_result%
-md %out_doxygen_xml%
-md %out_doxygen_yml%
-md %out_filelist%
+
+rd /s /q "%out_doxygen_root%"
+rd /s /q "%out_doxygen_mk%"
+rd /s /q "%out_doxygen_result%"
+rd /s /q "%out_doxygen_xml%"
+rd /s /q "%out_doxygen_yml%"
+rd /s /q "%out_filelist%"
+
+md "%out_doxygen_root%"
+md "%out_doxygen_mk%"
+md "%out_doxygen_result%"
+md "%out_doxygen_xml%"
+md "%out_doxygen_yml%"
+md "%out_filelist%"
 
 rem --------------------------------------------------------------------------
 rem start terminal
