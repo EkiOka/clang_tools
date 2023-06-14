af0f41278b4ddd889a6c2e1beb6395635d531f6a
EkiOka <96645770+EkiOka@users.noreply.github.com>
2023-06-15 07:17:01 +0900

    ucpyをライブラリ構造変更に対応

--- 00_fix/lib/lib_40d60c18793c4117a0e52c0fdadfd4fc/apps/cmd_update_copy.py
+++ 00_fix/lib/lib_40d60c18793c4117a0e52c0fdadfd4fc/apps/cmd_update_copy.py
@@ -1,41 +1,42 @@
-import lib_40d60c18793c4117a0e52c0fdadfd4fc.apps as lib
+import lib_40d60c18793c4117a0e52c0fdadfd4fc.apps.cmd_app_basic as cab
+import lib_40d60c18793c4117a0e52c0fdadfd4fc.adps.adp as a
+import lib_40d60c18793c4117a0e52c0fdadfd4fc.path_list as pl
 
-class cmd_update_copy(lib.cmd_app):
+class cmd_update_copy(cab.cmd_app):
     def copy_update_file(s,src:str,dest:str)->bool:
         """ファイルの内容に変化がある場合は更新のため上書きコピーします
         日付の変更のみの場合はコピーしません。
         ファイルパスの指定にはマスクは使えません。
         """
-        u = s.base_cmd_app.utility
-        if not(u.is_exist(src)):
-            u.raise_FileNotFound(src)
+        if not(a.is_exist(src)):
+            a.raise_FileNotFound(src)
         run = False
-        if not(u.is_exist(dest)):
-            u.log_debug(f"copy_update_file > copy(not exits `{dest}`)")
-            u.copy_file(src,dest)
+        if not(a.is_exist(dest)):
+            a.log_debug(f"copy_update_file > copy(not exits `{dest}`)")
+            a.copy_file(src,dest)
             run = True
         else:
-            src_size = u.file_size(src)
-            dst_size = u.file_size(dest)
+            src_size = a.get_file_size(src)
+            dst_size = a.get_file_size(dest)
 
             if not(src_size==dst_size):
-                u.log_debug(f"copy_update_file > copy(file size not equal)")
-                u.copy_file(src,dest)
+                a.log_debug(f"copy_update_file > copy(file size not equal)")
+                a.copy_file(src,dest)
                 run = True
             else:
-                src_time = u.file_update_time(src)
-                dest_time = u.file_update_time(dest)
+                src_time = a.get_file_update_time(src)
+                dest_time = a.get_file_update_time(dest)
                 if not(src_time==dest_time):
-                    src_sha  = u.file_sha256(src)   
-                    dst_sha  = u.file_sha256(dest)   
+                    src_sha  = a.get_file_sha256(src)   
+                    dst_sha  = a.get_file_sha256(dest)   
                     if not(src_sha==dst_sha):
-                        u.log_debug(f"copy_update_file > not copy(SHA equal)")
-                        u.copy_file(src,dest)
+                        a.log_debug(f"copy_update_file > not copy(SHA equal)")
+                        a.copy_file(src,dest)
                         run = True
                     else:
-                        u.log_debug(f"copy_update_file > not copy(SHA equal)")
+                        a.log_debug(f"copy_update_file > not copy(SHA equal)")
                 else:
-                    u.log_debug(f"copy_update_file > not copy(timestamp equal)")
+                    a.log_debug(f"copy_update_file > not copy(timestamp equal)")
         return run
     @staticmethod
     def gen_path_app():
