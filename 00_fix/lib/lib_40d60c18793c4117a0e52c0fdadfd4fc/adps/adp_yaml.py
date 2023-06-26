5bfcbd283b206081bf571b9a421324c028cd3779
EkiOka <96645770+EkiOka@users.noreply.github.com>
2023-06-26 20:15:41 +0900

    yamlキーソートして保存

--- 00_fix/lib/lib_40d60c18793c4117a0e52c0fdadfd4fc/adps/adp_yaml.py
+++ 00_fix/lib/lib_40d60c18793c4117a0e52c0fdadfd4fc/adps/adp_yaml.py
@@ -1,19 +1,23 @@
+# リリース時にはコメントアウトしてください
+# import yaml
 import lib_40d60c18793c4117a0e52c0fdadfd4fc.pkgs as pkgs
 
 yaml = None
-# リリース時にはコメントアウトしてください
-#import yaml
+
 
 def __import():
     global yaml
     if yaml == None:
         yaml = pkgs.imp("yaml")
 
+
 def safe_load(file) -> dict:
     __import()
     return yaml.safe_load(file)
 
-def dump(obj,file, allow_unicode:bool=True,default_flow_style:bool=False):
+
+def dump(obj, file, allow_unicode: bool = True, default_flow_style: bool = False, sort_keys=True):
     __import()
-    yaml.dump(obj,file, allow_unicode=allow_unicode, default_flow_style=default_flow_style)
+    yaml.dump(obj, file, allow_unicode=allow_unicode,
+              default_flow_style=default_flow_style, sort_keys=sort_keys)
     return
