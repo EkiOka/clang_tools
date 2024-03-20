3ddc57fcd8156c55b7100879bec24cb5211933ba
EkiOka <myxuomopppmo21uhcqlc@yahoo.co.jp>
2024-03-20 09:01:37 +0900

    コンフィグバッチない場合のメッセージ追加、体裁調整、サブルーチン化

--- .clt/hooks/clt_hook_vscode_terminal.bat
+++ .clt/hooks/clt_hook_vscode_terminal.bat
@@ -21,12 +21,36 @@ echo %cur_exe_name% ^> clt_env_id    : %clt_env_id%
 echo %cur_exe_name% ^> clt_dir       : %clt_dir%
 echo %cur_exe_name% ^> clt_hooks_dir : %clt_hooks_dir%
 
-call %clt_hooks_dir%\clt_config_path.bat
-call %clt_term_cfg_path%
-call %clt_hooks_dir%\clt_config_path_post.bat
-call %clt_term_cfg_path_post%
+call :run_bat "%clt_hooks_dir%\clt_config_path.bat" ""
+call :run_bat "%clt_term_cfg_path%" "If you use a path other than the default, please create the this file."
+call :run_bat "%clt_hooks_dir%\clt_config_path_post.bat" ""
+call :run_bat "%clt_term_cfg_path_post%" "If you use a path other than the default, please create the this file."
 
-set clt_
+call :disp_and_run "set clt_"
+
+goto :end_proc
+rem ----------------------------------------------------------------------
+:run_bat
+rem ----------------------------------------------------------------------
+if exist "%~1" (
+    call %~1
+) else (
+    echo %cur_exe_name% ^> %~1 is not fonund.
+    echo %cur_exe_name% ^> %~2
+    echo %cur_exe_name% ^> 
+)
+exit /b 0
+
+rem ----------------------------------------------------------------------
+:disp_and_run
+rem ----------------------------------------------------------------------
+echo %cur_exe_name% ^> %~1
+echo ---------------------------------------------------------------------
+echo.
+%~1
+echo.
+echo ---------------------------------------------------------------------
+exit /b 0
 
 rem ----------------------------------------------------------------------
 :end_proc
